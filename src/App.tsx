21f778e43d66337a39b7d6a9d3205d8906414243
Mary Washburn <mary@Wills-Personal-2017-MacBook.local>
2023-11-04 16:29:43 -0400

    Add boxes and start styling home page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ const App = () => {
   return (
     <>
       <Navigation />
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center justify-center">
+          <h1>Hello, My Name is Mary Washburn.</h1>
+          <h2>Nice to see you!</h2>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex">
+            <div className="p-4">box 1</div>
+            <div className="p-4">box 2</div>
+          </div>
+          <div className="flex">
+            <div className="p-4">box 3</div>
+            <div className="p-4">box 4</div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
