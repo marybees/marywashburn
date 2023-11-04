96c0922813f3e0eed1abdba5eb9b76c93c92500b
Mary Washburn <mary@Wills-Personal-2017-MacBook.local>
2023-11-04 15:25:00 -0400

    Create a Home component and move Navigation and Footer into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+
 const App = () => {
 
   return (
-    <div className="flex flex-col">
-      <h1 className="text-3xl font-bold">
-        Hello,
-      </h1>
-      <h1>My Name is Mary</h1>
-      <h1>Nice to see you!</h1>
-    </div>
+    <>
+      <Navigation />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-3xl font-bold">
+                Hello,
+            </h1>
+            <h1>My Name is Mary</h1>
+            <h1>Nice to see you!</h1>
+        </div>
+    )
+}
